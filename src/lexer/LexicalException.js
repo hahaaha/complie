ca515c6adb58ac6e546359f342c0aefaebee554d
haha <390519764@qq.com>
2021-04-01 23:44:53 +0800

    chore: init project

---
+++ src/lexer/LexicalException.js
@@ -0,0 +1,11 @@
+class LexicalException extends Error {
+    constructor(msg) {
+        super(msg)
+    }
+
+    static formChar(c) {
+        return new LexicalException(`unexpected char ${c}`)
+    }
+}
+
+module.exports = LexicalException
